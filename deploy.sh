50a5a617aff49b9b20e065d92407a39fc439ea56
Sampsa Laapotti <sampsa@laapotti.net>
2015-03-25 14:57:36 +0000

    deploy updates

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 # Not tested, so don't trust this script
 cd /home/pi/
 git clone github:sampoz/symfoni-hockey.git
+cd symfoni-hockey
 sudo cp rc.local_example /etc/rc.local
 crontab crontab # install crontab
+sudo apt-get install python-pip
+sudo pip install -r requirements.txt
+sudo apt-get install python-serial
--- runSnow.sh
+++ runSnow.sh
@@ -5,6 +5,6 @@
 if ps -efa | grep -v grep | grep snow_serial ; then
         exit 0
 else
-	sudo python /home/pi/symfoni-hockey/snow_serial.py >> /home/pi/symfoni-hockey/snow.log & 
+	sudo python /home/pi/symfoni-hockey/snow_serial.py > /home/pi/symfoni-hockey/snow.log & 
         exit 0
 fi
