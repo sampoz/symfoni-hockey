5da3bb797da791b53a5c7888458c9831d4106c37
Sampsa Laapotti <sampsa@laapotti.net>
2015-04-20 15:23:24 +0000

    fixed wifi

--- runSnow.sh
+++ runSnow.sh
@@ -5,6 +5,8 @@
 if ps -efa | grep -v grep | grep snow_serial ; then
         exit 0
 else
+	sudo ifdown wlan0
+	sudo ifup wlan0
 	sudo python /home/pi/symfoni-hockey/snow_serial.py > /home/pi/symfoni-hockey/snow.log & 
         exit 0
 fi
